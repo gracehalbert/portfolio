cd33a425b905970712f0d29f5fe49e03a04edd47
gracehalbert <gracehalbert@gmail.com>
2017-02-07 13:51:12 -0600

    add html

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
 app.get('/', function(req, res) {
-  res.send('hello world');
+  res.render('/public/index.html');
 });
 
 app.listen(port, function() {
